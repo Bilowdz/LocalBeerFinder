c884f1ba36789573f167a1d7351ed37245f0d6e5
Bilowdz <simonbilodeau27@gmail.com>
2024-05-19 04:54:02 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Map of beer goes here
+                </p>
+            </header>
+        </div>
+    )
+}
+
+export default App;
